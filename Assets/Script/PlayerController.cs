feae2aa504b01e87cace5873c10df676b5d07b3b
Shinochi-Haru <haru060109011305@gmail.com>
2023-05-27 00:04:51 +0900

    [add]playerの作成

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _upPower = 10f;
+    [SerializeField] float _horizonPower = 10f;
+    private Vector3 moveDirection; // キャラクターの移動方向
+
+    Rigidbody _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        // 上下左右のキーの入力を取得
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // 移動する方向を決める
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // 移動する向きにキャラクターの向きを変える
+        if (direction.magnitude >= 0.1f)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+        }
+
+        // Rigidbodyに力を加える
+        Vector3 movement = direction * _horizonPower * Time.deltaTime;
+        _rb.AddForce(movement, ForceMode.VelocityChange);
+        //上昇する
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * _upPower);
+        }
+        else
+        {
+            _rb.velocity = Vector3.zero;
+        }
+    }
+}

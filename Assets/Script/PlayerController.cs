7a616e8c91dfeb1cb9cc0afafbd99d823b55ecbe
Shinochi-Haru <haru060109011305@gmail.com>
2023-06-05 18:15:57 +0900

    [add] 地形の生成

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,44 +4,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float _upPower = 10f;
-    [SerializeField] float _horizonPower = 10f;
-    private Vector3 moveDirection; // キャラクターの移動方向
+    public float speed = 10.0f;                     // キャラクターの移動速度
+    public float sensitivity = 30.0f;               // マウスの感度
+    public float WaterHeight = 15.5f;               // 水面の高さ
+    Rigidbody rb;                                   // Rigidbody コンポーネントへの参照
+    public GameObject cam;                          // カメラオブジェクトへの参照
+    float moveFB, moveLR;                            // 前後左右の移動量
+    float rotX, rotY;                                // マウスの回転量
+    public bool webGLRightClickRotation = true;      // WebGL において右クリックでの回転を有効にするかのフラグ
+    float gravity = -9.8f;                           // 重力の値
 
-    Rigidbody _rb;
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();                 // 自身にアタッチされているRigidbodyコンポーネントを取得
+        if (Application.isEditor)
+        {
+            webGLRightClickRotation = false;                // エディタ上では WebGL での右クリック回転を無効化
+            sensitivity = sensitivity * 1.5f;               // エディタ上では感度を1.5倍に増やす
+        }
     }
 
-    // Update is called once per frame
+    void CheckForWaterHeight()
+    {
+        if (transform.position.y < WaterHeight)
+        {
+            gravity = 0f;                                  // キャラクターが水面より下にいる場合は重力を無効化
+        }
+        else
+        {
+            gravity = -9.8f;                               // キャラクターが水面より上にいる場合は重力を有効化
+        }
+    }
+
     void Update()
     {
+        moveFB = Input.GetAxis("Horizontal") * speed;      // 水平方向（左右）の入力に基づいて移動量を計算
+        moveLR = Input.GetAxis("Vertical") * speed;        // 垂直方向（前後）の入力に基づいて移動量を計算
 
+        rotX = Input.GetAxis("Mouse X") * sensitivity;     // マウスの X 軸移動量に基づいて横回転量を計算
+        rotY = Input.GetAxis("Mouse Y") * sensitivity;     // マウスの Y 軸移動量に基づいて縦回転量を計算
+
+        CheckForWaterHeight();                             // 水面の高さをチェックし、必要に応じて重力を調整
+
+        Vector3 movement = new Vector3(moveFB, gravity, moveLR);   // 移動量をベクトルとして設定
+
+        if (webGLRightClickRotation)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                CameraRotation(cam, rotX, rotY);             // WebGL での右クリック回転が有効な場合、マウスの移動量に基づいてカメラを回転
+            }
+        }
+        else if (!webGLRightClickRotation)
+        {
+            CameraRotation(cam, rotX, rotY);                 // WebGL での右クリック回転が無効な場合、常にマウスの移動量に基づいてカメラを回転
+        }
+
+        movement = transform.rotation * movement;            // 移動量をキャラクターのローカル座標系に変換
+        rb.velocity = movement;                              // Rigidbodyの速度を設定
     }
 
-    private void FixedUpdate()
+    void CameraRotation(GameObject cam, float rotX, float rotY)
     {
-        // 上下左右のキーの入力を取得
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        // 移動する方向を決める
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        // 移動する向きにキャラクターの向きを変える
-        if (direction.magnitude >= 0.1f)
-        {
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
-        }
-
-        // Rigidbodyに力を加える
-        Vector3 movement = direction * _horizonPower * Time.deltaTime;
-        _rb.AddForce(movement, ForceMode.VelocityChange);
-        //上昇する
-        if (Input.GetKey(KeyCode.Space))
-        {
-            _rb.AddForce(Vector3.up * _upPower,ForceMode.Impulse);
-        }
+        transform.Rotate(0, rotX * Time.deltaTime, 0);       // キャラクターを横回転
+        cam.transform.Rotate(-rotY * Time.deltaTime, 0, 0);  // カメラを縦回転
     }
 }
